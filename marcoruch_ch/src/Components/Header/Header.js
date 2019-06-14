f2ba773dce3186cb84c2e88d45ab0068c7b581f0
Marco Ruch <marco_ruch@gmx.ch>
2019-06-14 13:20:41 +0200

    Fixed Overflow Bug for Smartphone Users

--- marcoruch_ch/src/Components/Header/Header.js
+++ marcoruch_ch/src/Components/Header/Header.js
@@ -6,15 +6,26 @@ import './Header.scss';
 
 
 import firebase from './../Firebase/Firebase';
-const extraWidth = {
-    width:'400px',
-    color: 'white'
-}
+
 
 function Header() {
     const [user, setUser] = useState(null);
+    const [hamburger, setHamburger] = useState(false);
 
+    const login = {
+        width:'400px',
+        color: 'white',
+        float:'right',
+        marginLeft: 'auto'
+    }
+
+    const hamburgerMenuStyle = {
+        display: hamburger ? "block" : "none",
+    }
+
+    
     firebase.auth().onAuthStateChanged((user) => {
+        console.log(user);
         if (user) {
             setUser(user);
         } else {
@@ -24,6 +35,7 @@ function Header() {
 
     return (
         <header className="App-header">
+            <div className="large">
             <ul>
                 <li>
                     <Link to="/">Home</Link>
@@ -43,10 +55,39 @@ function Header() {
                 <li>
                     <Link to="/contact">Contact</Link>
                 </li>
-                <li style={extraWidth}>
+                <li style={login}>
                     {user ? <UserControlPanelNav /> : <LoginNav />}
                 </li>
-    </ul> 
+    </ul> </div>
+    <div className="medium">
+            <div  id="hamburger-holder" onClick={()=>setHamburger(!hamburger)}>
+                <div class="hamburger"></div>
+                <div class="hamburger"></div>
+                <div class="hamburger"></div>
+            </div>
+            <ul style={hamburgerMenuStyle}>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+            <li>
+                 <Link to="/projects">Projects</Link>
+                </li>
+                <li>
+                    <Link to="/skills">Skills</Link>
+                </li>
+                <li>
+                    <Link to="/games">Games</Link>
+                </li>
+                <li>
+                    <Link to="/blog">Blog</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+                <li >
+                    {user ? <UserControlPanelNav /> : <LoginNav />}
+                </li>
+    </ul> </div>
         </header>
     );
 }
