a5251bf55163b687f5b9a35a72e6a86f500cb761
marco.ruch <marco.ruch@stud.bbbaden.ch>
2019-06-18 01:39:51 +0200

    Added Dotsmatrix basics for game field of Muehle

--- marcoruch_ch/src/Components/Muehle/Muehle.jsx
+++ marcoruch_ch/src/Components/Muehle/Muehle.jsx
@@ -3,10 +3,18 @@ import "./Muehle.scss"
 
 const dots = [1,2,3,4,5,6,7,8]
 
+const dots_matrix =
+[
+    [1,0,0,1,0,0,1],
+    [0,1,0,1,0,1,0],
+    [0,0,1,1,1,0,0],
+    [1,1,1,0,1,1,1],
+    [0,0,1,1,1,0,0],
+    [0,1,0,1,0,1,0],
+    [1,0,0,1,0,0,1],
+]
 
 function Muehle() {
-
-
     return (
         <React.Fragment>
             <div className="muehle">
@@ -15,11 +23,11 @@ function Muehle() {
                 </div>
                 <div className="game">
                     <div className="outer-field">
-                    {dots.map(i => <div className="dot"></div>) }
+                    {dots.map(i => <div className="dot" draggable></div>) }
                         <div className="middle-field">
-                    {dots.map(i => <div className="dot"></div>) }
+                    {dots.map(i => <div className="dot" draggable></div>) }
                             <div className="center-field">
-                    {dots.map(i => <div className="dot"></div>) }
+                    {dots.map(i => <div className="dot" draggable></div>) }
                             </div>
                         </div>
                     </div>
