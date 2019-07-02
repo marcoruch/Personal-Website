f681f983112aa5769de8cc55ad3a1f218f0a6397
marcoruch <marco_ruch@gmx.ch>
2019-07-02 12:12:07 +0200

    Some more CV Functionality
    Co-Authored-By: Marco Ruch <marco_ruch@gmx.ch>

--- marcoruch_ch/src/Components/CurriculumVitae/CurriculumVitae.jsx
+++ marcoruch_ch/src/Components/CurriculumVitae/CurriculumVitae.jsx
@@ -1,52 +1,133 @@
 
 import React, { useState, useEffect } from 'react';
-import './CurriculumVitae.scss';
-import Me from './../../Content/img/programmer.jpg'
+import Me from './../../Content/img/marcoruch_cv.jpg'
 import { Loader } from 'semantic-ui-react'
+import HistoryPart from './HistoryPart/HistoryPart'
 import firebase from './../Firebase/Firebase'
-import HistoryPart from './HistoryPart/HistoryPart.jsx'
+
+import './CurriculumVitae.scss';
 
 function CurriculumVitae() {
 
     const [historyParts, setHistoryParts] = useState(null);
+    const [type, setType] = useState(null);
+    const [min, setMin] = useState(null);
+    const [max, setMax] = useState(null);
 
     async function fetchHistoryParts() {
+
         const fetchedHistoryParts = [];
-        await firebase.firestore().collection('historyParts').orderBy("val", "desc").get()
+        await firebase.firestore().collection('historyParts').orderBy("to", "desc").get()
         .then(function(querySnapshot) {
             querySnapshot.forEach(function(doc) {
                 fetchedHistoryParts.push(doc.data());
             });
         })
         setHistoryParts(fetchedHistoryParts);
+
+        let min= null;
+        let max= null;
+
+        for (let index = 0; index < fetchedHistoryParts.length; index++) {
+            const element = fetchedHistoryParts[index];
+            let span = null;
+
+            if (element.to){
+                span =element.to.seconds - element.from.seconds;
+            } else {
+                span = Math.floor(Date.now() / 1000) - element.from.seconds; 
+            }
+
+            if (span > max || max === null){
+                max = span;
+            } else if (span < min || min === null){
+                min = span;
+            } 
+        }
+
+        setMin(min);
+        setMax(max);
     }
 
     useEffect(() => {
         fetchHistoryParts();
     }, []);
 
-    const historyFadeStyle = (index) => {
-        return {animation: `fadeHistoryPartIn ${index*0.3}s forwards`,}
+
+    const unique = (arr) => {
+        return arr.filter(function (value, index, self) { 
+          return self.indexOf(value) === index;
+        });
+      }
+
+    const handleTypeChosen = (type) =>{
+        setType(type);
     }
 
-    return !historyParts
-        ? <div  className="skillsloader"><Loader active inline='centered' /></div>
-        :<React.Fragment>
-        <div className="curriculumVitae">
+    const historyFadeStyle = (index) => {
+        return {
+            opacity: 0,
+            animation: `fadeHistoryPartIn 0.3s  forwards`,
+            animationDelay: `${index*0.3}s`
+        }
+    }
+
+    const bubbleStyle = (factor) => {
+        let biggest = 200 * 0.6;
+        return {width: biggest*factor, height: biggest* factor}
+    }
+
+
+    const normalize = (element) => {
+        let span = null;
+        if (element.to){
+            span =element.to.seconds - element.from.seconds;
+        } else {
+            span = Math.floor(Date.now() / 1000) - element.from.seconds; 
+        }
+
+        return (span - min) / (max - min)+0.4;
+    }
+
+    return <div className="curriculumVitae">
             <div className="header">
-                <img  alt="lol" src={Me}></img>
+                <div className="picture-holder">
+                    <img  alt="CVpicture" src={Me}></img>
+                    <span role="img" aria-label="heart" className="imgAfter">❤️</span>
+                </div>
                 <div className="meta">
                 <h1>Marco Ruch</h1>
                 <h2>Junior Software Engineer</h2>
                 <h3>Aarau, Schweiz</h3></div>
             </div>
-            <div className="history">
-                {historyParts.map((item,index) => <HistoryPart item={item} historyFadeStyle={index}></HistoryPart>)}
-            </div>
+            {
+                historyParts === null
+                ? <div  className="historyloader"><Loader active inline='centered' /></div>
+                : <div className="history">
+                  
+                     <div className="history-header">
+                    {    unique(historyParts.map(historyPart=> historyPart.type)).map((historyPartType) =>
+                    <div className={type && historyPartType === type ? "type-small active" : "type-small"  } onClick={() => handleTypeChosen(historyPartType)}>{historyPartType}</div>
+                        )}
+                        <div className={type === null ? "type-small active" : "type-small"  } onClick={() => handleTypeChosen(null)}>Alle anzeigen</div>
+                
+                         </div>
+                  
+                    {
+                        historyParts
+                        .filter(item => type === null || item.type === type)
+                        .map((item,index) => 
+                         <HistoryPart 
+                         key={index} 
+                         historyPart={item} 
+                         bubbleStyle={bubbleStyle(normalize(item))} 
+                         historyFadeStyle={historyFadeStyle(index)}></HistoryPart>)
+                    }
+                </div>
+            }
         </div>
-    </React.Fragment>
+        
 };
-
 
 
 export default CurriculumVitae;
--- marcoruch_ch/src/Components/CurriculumVitae/HistoryPart/HistoryPart.jsx
+++ marcoruch_ch/src/Components/CurriculumVitae/HistoryPart/HistoryPart.jsx
@@ -2,10 +2,23 @@
 import React, { useState, useEffect } from 'react';
 import './HistoryPart.scss';
 
-function HistoryPart() {
-
-    return <React.Fragment>
-    </React.Fragment>
+function HistoryPart(props) {
+    return <div className="historyPart" style={props.historyFadeStyle}>
+        <div className="history-left">
+        <div className="timeSpentDisplay" style={{fontSize: Math.ceil(props.bubbleStyle.width/ 10) + 10}}>{props.historyPart.timeSpent}</div>
+        <div className="bubbleConnector"></div>
+        <div className="bubble" style={props.bubbleStyle}>
+        </div>
+        </div>
+        <div className="history-middle">
+            <h1>{props.historyPart.place}</h1>
+            <h2>{props.historyPart.desc}</h2>
+        </div>
+        <div className="history-right">
+            
+        </div>
+        
+    </div>
 };
 
 
