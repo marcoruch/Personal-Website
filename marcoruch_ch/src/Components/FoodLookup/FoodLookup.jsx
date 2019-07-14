78f5517f369b1d6a5cd283a1329508ec0000d782
marco.ruch <marco.ruch@stud.bbbaden.ch>
2019-07-14 23:14:10 +0200

    Save ProgressCommit
    Co-Authored-By: marcoruch <marcoruch@users.noreply.github.com>

--- marcoruch_ch/src/Components/FoodLookup/FoodLookup.jsx
+++ marcoruch_ch/src/Components/FoodLookup/FoodLookup.jsx
@@ -1,6 +1,6 @@
 
-import React from 'react';
-import { Dropdown, Menu } from 'semantic-ui-react'
+import React, { useState } from 'react';
+import { Dropdown, Menu, Icon, Button } from 'semantic-ui-react'
 import GoogleApiWrapper from '../MapContainer/MapContainer'
 
 import './FoodLookup.scss';
@@ -9,19 +9,40 @@ import './FoodLookup.scss';
 
 function FoodLookup() {
 
-    const options = [
-        { key: 1, text: 'Choice 1', value: 1 },
-        { key: 2, text: 'Choice 2', value: 2 },
-        { key: 3, text: 'Choice 3', value: 3 },
-      ]
+    const [rating, setRating] = useState(null);
+    const [local, setLocal] = useState(true);
+    const [searchPlaceOptions, setSearchPlaceOptions] = useState({});
 
-      
+    const searchBaseQuery = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=YOUR_API_KEY";
+    const starOptions = [
+        { key: 1, value: 1 },
+        { key: 2, value: 1.5 },
+        { key: 3, value: 2 },
+        { key: 4, value: 2.5 },
+        { key: 5, value: 3 },
+        { key: 6, value: 3.5 },
+        { key: 7, value: 4 },
+        { key: 8, value: 4.5 },
+        { key: 9, value: 5 },
+    ]
+
+
+    const handleLazyLoadingDropDownInputChanged = (options, inputQuery) => {
+        console.log(inputQuery)
+        const newFoundOptions = {};
+        setSearchPlaceOptions(newFoundOptions);
+        return options.filter(opt => true)
+    }
+
+
+
     const navStyles = {
         width: '30%',
         position: 'absolute',
         left: '0',
         top: '100px',
         bottom: '-100px',
+        zIndex: 1000,
     }
 
 
@@ -32,7 +53,32 @@ function FoodLookup() {
         position: 'absolute',
         left: '30%',
         top: '0',
+        zIndex: 999,
     };
+
+    const optionsStyle = {
+        display: 'flex',
+        flexDirection: 'row'
+    }
+
+    const optionsLabelStyle = { textAlign: 'center', width: '25px', marginRight: '10px' }
+    const handleRatingSelected = (stars) => {
+        if (stars !== rating) {
+            setRating(stars)
+        }
+    }
+    const setSearchLocal = (searchLocal) => {
+        setLocal(searchLocal);
+    }
+
+    const getStars = (starAmount) => {
+        const fullStars = Math.floor(starAmount);
+        const halfStars = starAmount - fullStars;
+        const stars = Array(fullStars).fill(<Icon name="star"></Icon>);
+        if (halfStars > 0) stars.push(<Icon name="star half"></Icon>)
+        console.log(stars);
+        return stars;
+    }
 
     return <div className="food-lookup">
         <div className='nav-left' style={navStyles}>
@@ -40,9 +86,30 @@ function FoodLookup() {
             <h2>Choose what you want and click submit.</h2>
 
             <div className="filters">
-                <Menu compact>
-                    <Dropdown text='Dropdown' options={options} simple item />
-                </Menu>
+                <Button.Group>
+                    {local ? <Button onClick={() => setSearchLocal(true)} inverted color='green'>Jetzige Umgebung </Button> : <Button onClick={() => setSearchLocal(true)} color='red'>Jetzige Umgebung</Button>}
+                    <Button.Or />
+                    {local ? <Button onClick={() => setSearchLocal(false)} inverted color='red'>Anderer Ort</Button> : <Button color='green' onClick={() => setSearchLocal(false)} >Anderer Ort</Button>}
+                </Button.Group>
+                {!local ? <Dropdown
+                    fluid
+                    options={searchPlaceOptions}
+                    placeholder='Try to search for case or CASE'
+                    search={handleLazyLoadingDropDownInputChanged}
+                    selection
+                /> : <React.Fragment></React.Fragment>}
+                <Dropdown text={rating ? `Mindestens ${rating} Sterne` : "Mindestbewertung"} icon='star' floating labeled button className='icon'>
+                    <Dropdown.Menu>
+                        <Dropdown.Header icon='google' content='1-5 Sterne' />
+                        <Dropdown.Divider />
+                        {starOptions.map(starOption =>
+                            <Dropdown.Item
+                                text={<div
+                                    style={optionsStyle}><div
+                                        style={optionsLabelStyle}>{starOption.value}</div>
+                                    {getStars(starOption.value).map(star => star)}</div>} onClick={() => handleRatingSelected(starOption.value)} />)}
+                    </Dropdown.Menu>
+                </Dropdown>
             </div>
         </div>
         <GoogleApiWrapper mapStyles={mapStyles}></GoogleApiWrapper>
