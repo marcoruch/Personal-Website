a306cc3661a21ada9311e41fd2a84fd1c79c7272
marcoruch <marco_ruch@gmx.ch>
2019-08-06 11:32:54 +0200

    Footer.js Added new Freepik Image
    Co-Authored-By: Marco Ruch <marco_ruch@gmx.ch>

--- marcoruch_ch/src/Components/Footer/Footer.js
+++ marcoruch_ch/src/Components/Footer/Footer.js
@@ -10,6 +10,7 @@ function Footer() {
         <a href="https://de.freepik.com/fotos-vektoren-kostenlos/design">Design Vektor erstellt von freepik - de.freepik.com</a>
         <a href="https://www.freepik.com/free-photos-vectors/background">Background vector created by freepik - www.freepik.com</a>
         <a href="https://de.freepik.com/fotos-vektoren-kostenlos/reise">Reise Vektor erstellt von rawpixel.com - de.freepik.com</a>
+        <a href="https://www.freepik.com/free-photos-vectors/background">Background vector created by rawpixel.com - www.freepik.com</a>
 
         </div>
         </footer>
