305fa22a59703a58dd205c4c6f352387e30e81f5
marcoruch <marco_ruch@gmx.ch>
2019-10-13 03:11:34 +0200

    Finished Admin Auto-Entry-Formulars
    Co-Authored-By: Marco Ruch <marco_ruch@gmx.ch>

--- marcoruch_ch/src/Components/TopLevelEntryFormular/TopLevelEntryFormular.jsx
+++ marcoruch_ch/src/Components/TopLevelEntryFormular/TopLevelEntryFormular.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import API_HOST from '../../environment'
 import { Form, Button, Icon } from 'semantic-ui-react'
-
+import Swal from 'sweetalert2'
 import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
@@ -13,8 +13,8 @@ import "./TopLevelEntryFormular.scss";
 
 function TopLevelEntryFormular(props) {
 
-    const [NewObject, SetNewObject] = useState(new FormData());
     const [ConfigurationKey, setConfigurationKey] = useState(props.EntryKey)
+    const [NewObject, SetNewObject] = useState({});
     const [FormConfiguration, setFormConfiguration] = useState(null)
     const [CantLoad, setCantLoad] = useState(false)
 
@@ -26,7 +26,6 @@ function TopLevelEntryFormular(props) {
     const getLineWithField = (Field, Title) => {
         return <div className="input-line"><h4>{Title}</h4>{Field}</div>
     }
-
 
     const getStringInputField = (name, readableName) => {
         return <Form.Input className={"input-string"} onChange={(_, { value }) => setConfigValueToProps(name, value)} placeholder={readableName}></Form.Input>
@@ -47,7 +46,7 @@ function TopLevelEntryFormular(props) {
                 <Button.Content visible>{`${NewObject[name] == null ? "Hochladen" : "Datei bereit für Upload"}`}</Button.Content>
                 <Button.Content hidden> {NewObject[name] == null ? <Icon name='upload' /> : <Icon color='green' name='check'></Icon>} </Button.Content>
             </Button>
-            <input ref={fileInputRef} type="file" hidden onChange={(fileChange) => (fileChange.target.files ? setImageToProps(name, fileChange.target.files[0]) : console.log("Cancel"))} />
+            <input ref={fileInputRef} type="file" hidden onChange={(fileChange) => (fileChange.target.files ? setImageToProps(name, fileChange.target.files[0], readableName) : console.log("Cancel"))} />
         </React.Fragment>
     }
 
@@ -71,37 +70,47 @@ function TopLevelEntryFormular(props) {
         }
     }
 
-    const setImageToProps = (configKey, file) => {
-        SetNewObject(({ ...NewObject, [configKey]: file }));
+    const toBase64 = (file) => new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+
+    const setImageToProps = async (configKey, file, readableName) => {
+        const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
+        if (file) {
+            if (!acceptedImageTypes.includes(file['type'])) {
+                Swal.fire(`Fehler beim Hochladen von ${readableName}`, `Kein Bildtyp für die Datei ${file.name} erkannt.`, 'error')
+            } else {
+                let fileBase64 = await toBase64(file);
+                SetNewObject(({ ...NewObject, [configKey]: { file: fileBase64, fileName: file.name, type: file.type } }));
+            }
+        } else {
+            console.log(`Fehler beim Hochladen von ${readableName}`);
+            console.log(`Keine Datei gefunden`);
+        }
     }
 
     const setConfigValueToProps = (configKey, configValue) => {
         SetNewObject(({ ...NewObject, [configKey]: configValue }));
     }
 
-    const handleSubmit = async () => {
-        let data = new FormData();
 
-        let array = [];
-        Object.keys(NewObject).forEach(key => {
-            if (FormConfiguration.configFields[key].type === "image") {
-                array.push({key: key, obj: NewObject[key], name: NewObject[key].fileName})
-            } else {
-                data.set(key, NewObject[key])
+    const onObjectPosted = (postedObject) => {
+        if (props.onObjectPosted){
+            props.onObjectPosted(postedObject);
+        }
+    }
+
+
+    const handleSubmit = async () => {
+        let postTopLevelEntryUrl = `${API_HOST}/api/topLevelEntry`;
+        axios.post(postTopLevelEntryUrl, NewObject).then(res => {
+            if (res.data) {
+                onObjectPosted(res.data);
             }
         });
-
-        array.forEach(item => {
-            data.append(item.key, item.obj, item.name);
-        });
-
-        let postedObject;
-        let postTopLevelEntryUrl = `${API_HOST}/api/topLevelEntry`;
-        axios.post(postTopLevelEntryUrl, data, { headers: {  'Content-Type': 'application/json',}}).then(res => {
-            console.log(res);
-            console.log(res.data);
-        });
-        return;
     }
 
     async function fetchConfiguration() {
@@ -122,6 +131,7 @@ function TopLevelEntryFormular(props) {
 
         if (fetchedConfiguration) {
             setFormConfiguration(fetchedConfiguration);
+            SetNewObject(({...NewObject, "configFields" : fetchedConfiguration.configFields, "entryKey" : ConfigurationKey}));
         } else {
             setRetriedFetching(retriedFetching + 1);
         }
