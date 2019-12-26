bf9d675bcaffd295bc174c16a4257ab7c598272c
marcoruch <marco_ruch@gmx.ch>
2019-12-27 00:04:14 +0100

    Rocket-Tracker Link from Game
    Co-Authored-By: Marco Ruch <marco_ruch@gmx.ch>

--- marcoruch_ch/src/Components/RocketTracker/RocketTracker.jsx
+++ marcoruch_ch/src/Components/RocketTracker/RocketTracker.jsx
@@ -4,12 +4,9 @@ import React, { useEffect, useState, useContext } from "react";
 import "./RocketTracker.scss";
 
 function RocketTracker() {
-
-
-
     return (
         <div className="rocket-tracker-holder">
-            <a href="https://its-noon-somewhere.firebaseapp.com">CLick me f</a>
+            <a href="https://its-noon-somewhere.firebaseapp.com">Rocket-Tracker öffnen</a>
         </div>
 
     )
