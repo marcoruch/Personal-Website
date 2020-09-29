eadc69c7b199622e9c3053a7dbcf6dbb67ba1669
Marco Ruch <marco_ruch@gmx.ch>
2020-09-29 22:04:55 +0200

    Additions scss
    Co-Authored-By: Marco Ruch <marco_ruch@gmx.ch>

--- marcoruch_ch/src/Components/OfficeTetris/OfficeTetris.jsx
+++ marcoruch_ch/src/Components/OfficeTetris/OfficeTetris.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Icon } from "semantic-ui-react";
 
 
 import "./OfficeTetris.scss";
@@ -6,7 +7,12 @@ import "./OfficeTetris.scss";
 function RocketTracker() {
     return (
         <div className="office-tetris-holder">
-            <p>This page is in work, so please visit the Application via the Link provided</p>
+            <p>
+            <Icon name="warning sign"></Icon> For Testing purposes you can register yourself and enter the dummy Team with the following Credentials <Icon name="warning sign"></Icon>
+            </p>
+            <pre className="highlight-1">Teamname: Dummy AG</pre>
+            <pre className="highlight-2">Passphrase: thankyou</pre>
+
             <a href="https://office-tetris.web.app/">Office-Tetris öffnen</a>
         </div>
     )
